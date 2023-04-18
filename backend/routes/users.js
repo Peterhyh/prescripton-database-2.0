0495cda11876eadba28362dc538f084aab47b3b5
Peter Huynh <peterhyh@yahoo.com>
2023-04-17 22:45:50 -0500

    authToken function added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,21 +9,21 @@ const router = express.Router();
 
 
 
-// function authToken(req, res, next) {
-//   const authHeader = req.headers['authorization']
-//   const token = authHeader && authHeader.split(' ')[1]
-//   if (token == null) {
-//     return res.sendStatus(401)
-//   }
+function authToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) {
+    return res.sendStatus(401)
+  }
 
-//   jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-//     if (err) {
-//       res.sendStatus(403)
-//     }
-//     req.user = user
-//     next()
-//   })
-// }
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      res.sendStatus(403)
+    }
+    req.user = user
+    next()
+  })
+}
 
 
 
