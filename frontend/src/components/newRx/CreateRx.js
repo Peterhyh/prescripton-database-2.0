8750e0b38cfb05ff703d1a90e9cbf7a780ebaf16
Peter Huynh <peterhyh@yahoo.com>
2023-06-08 20:45:28 -0500

    className cleanup on createRx

--- frontend/src/components/newRx/CreateRx.js
+++ frontend/src/components/newRx/CreateRx.js
@@ -45,10 +45,10 @@ const CreateRx = ({ uploadedRx, selectedLastName, selectedFirstName, setSelected
             <Alert className='d-flex justify-content-center' color='success' isOpen={openSuccess}>
                 Prescription saved successfully!
             </Alert>
-            <div className='dataentry-container'>
+            <div className='createRxContainer'>
 
-                <div className='dataentry-left'>
-                    <div className='dataentry-left-row-top'>
+                <div className='createRxLeftSideContainer'>
+                    <div className='uploadedRxContainer'>
                         <h1>Uploaded Rx:</h1>
                         <img src={uploadedRx} alt='uploaded rx' />
                     </div>
@@ -62,94 +62,84 @@ const CreateRx = ({ uploadedRx, selectedLastName, selectedFirstName, setSelected
                     </div>
                 </div>
 
-
-                <div className='dataentry-right'>
-                    <form >
-                        <h1 class='prescription-title'>PRESCRIPTION:</h1>
-                        <div className='row inputBox-drug'>
-                            <div class='col-sm-12 '>
-                                <input
-                                    name='drug'
-                                    type='text'
-                                    onChange={(e) => setDrugName(e.target.value)}
-                                    required
-                                />
-                                <span class='inputBox-drug-span'>Drug</span>
-                            </div>
+                <div className='createRxRightSideContainer'>
+                    <h1>Create Rx:</h1>
+                    <form className='createRxForm'>
+                        <div className='drugInputBoxContainer'>
+                            <input
+                                name='drug'
+                                type='text'
+                                onChange={(e) => setDrugName(e.target.value)}
+                                required
+                            />
+                            <span class='drugLabel'>Drug</span>
                         </div>
 
-                        <div className='row inputBox-sig '>
-                            <div class='col-sm-12 '>
-                                <input
-                                    name='direction'
-                                    type='text'
-                                    onChange={(e) => setDirection(e.target.value)}
-                                    required
-                                />
-                                <span class='inputBox-sig-span'>Direction</span>
-                            </div>
+                        <div className='directionInputBoxContainer'>
+                            <input
+                                name='direction'
+                                type='text'
+                                onChange={(e) => setDirection(e.target.value)}
+                                required
+                            />
+                            <span class='directionInputLabel'>Direction</span>
                         </div>
 
-                        <div className='inputBox-quanity-refills'>
-                            <div class='createRxQtyContainer'>
+                        <div className='qtyRefillDaySupplyContainer'>
+                            <div class='qtyInputContainer'>
                                 <input
                                     name='quanity'
                                     type='text'
                                     onChange={(e) => setQty(e.target.value)}
                                     required
                                 />
-                                <span class='inputBox-quanity-span'>Quantity</span>
+                                <span class='qtyLabel'>Quantity</span>
                             </div>
-                            <div class='createRxRefillContainer'>
+                            <div class='refillInputContainer'>
                                 <input
                                     name='refills'
                                     type='text'
                                     onChange={(e) => setRefill(e.target.value)}
                                     required
                                 />
-                                <span class='inputBox-refills-span'>Refills</span>
+                                <span class='refillLabel'>Refills</span>
                             </div>
-                            <div class='createRxDaySupplyContainer'>
+                            <div class='daySupplyInputContainer'>
                                 <input
                                     name='daySupply'
                                     type='text'
                                     onChange={(e) => setDaySupply(e.target.value)}
                                     required
                                 />
-                                <span class='inputBox-day-supply-span'>Day Supply</span>
+                                <span class='daySupplyLabel'>Day Supply</span>
                             </div>
                         </div>
-
-                        <div className='data-entry-button-container'>
-                            <button type='submit' outline>Submit</button>
-                        </div>
-
+                        <button className='createRxSubmitButton' type='submit' outline>Submit</button>
                     </form >
 
-                    <div className='dataEntryInstruction'>
-                        <div>
-                            <h1>Step 3:</h1>
-                            <ul>
-                                <li>
-                                    Here we will enter the prescription drug information and save it to the patient's profile.<br /><br />
-                                </li>
-                                <li>
-                                    Enter in the Drug name (Amoxicillin 250mg), direction (Take 2 tablets by mouth three times
-                                    daily for 7 days), quantity (#42), refills (0 by default if not specified), and the day's supply (7).<br /><br />
-                                </li>
-                                <li>
-                                    Once you have filled out the form completely, you are done with this prescription and ready to
-                                    move on. Let's check to see if the medication you submitted is now in the patient's profile. Click
-                                    "Patient Search" on the navigation bar above to continue.<br /><br />
-                                </li>
-                                <li>
-                                    In a professional setting, sometimes you will see the direction written in sig codes like the
-                                    example prescription here. Our job is to translate the codes into english or any language the
-                                    patient chooses. In this demo, we will use english.<br /><br />
-                                </li>
-                            </ul>
-                        </div>
+                    <div className='createRxInstruction'>
+                        <h1>Step 3:</h1>
+                        <ul>
+                            <li>
+                                Here we will enter the prescription drug information and save it to the patient's profile.<br /><br />
+                            </li>
+                            <li>
+                                Enter in the Drug name (Amoxicillin 250mg), direction (Take 2 tablets by mouth three times
+                                daily for 7 days), quantity (#42), refills (0 by default if not specified), and the day's supply (7).<br /><br />
+                            </li>
+                            <li>
+                                Once you have filled out the form completely, you are done with this prescription and ready to
+                                move on. Let's check to see if the medication you submitted is now in the patient's profile. Click
+                                "Patient Search" on the navigation bar above to continue.<br /><br />
+                            </li>
+                            <li>
+                                In a professional setting, sometimes you will see the direction written in sig codes like the
+                                example prescription here. Our job is to translate the codes into english or any language the
+                                patient chooses. In this demo, we will use english.<br /><br />
+                            </li>
+                        </ul>
                     </div>
+
                 </div>
             </div>
         </>
